c7d6e779e119fdaeef7696ce6c2691f5d4145936
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-03-21 20:24:17 -0500

    refactor: move board to its own package

---
+++ internal/board/board.go
@@ -0,0 +1,78 @@
+package board
+
+func New(row int, col int) [][]string {
+	board := make([][]string, row)
+	for i := range board {
+		board[i] = make([]string, col)
+	}
+	initialize(board)
+	return board
+}
+
+func initialize(board [][]string) {
+	for i := range board {
+		for j := range board[i] {
+			board[i][j] = " "
+		}
+	}
+	board[1][2] = "X"
+	board[2][3] = "X"
+	board[3][1] = "X"
+	board[3][2] = "X"
+	board[3][3] = "X"
+	board[4][2] = "X"
+	board[4][3] = "X"
+	board[5][2] = "X"
+}
+
+func getNeighbors(board [][]string, x int, y int) int {
+	var neighbors int
+	for i := -1; i < 2; i++ {
+		for j := -1; j < 2; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if x+i < 0 || x+i >= len(board) || y+j < 0 || y+j >= len(board[0]) {
+				continue
+			}
+			if board[x+i][y+j] == "X" {
+				neighbors++
+			}
+		}
+	}
+	return neighbors
+}
+
+// Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+// Any live cell with two or three live neighbors lives on to the next generation.
+// Any live cell with more than three live neighbors dies, as if by overpopulation.
+// Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+func Update(board [][]string) {
+	for i := range board {
+		for j := range board[i] {
+			neighbors := getNeighbors(board, i, j)
+			switch {
+			case neighbors < 2:
+				board[i][j] = " "
+			case neighbors == 2:
+				continue
+			case neighbors < 4:
+				board[i][j] = "X"
+			default:
+				board[i][j] = " "
+			}
+		}
+	}
+}
+
+func AsString(board [][]string) string {
+	var boardString string
+	for i := range board {
+		for j := range board[i] {
+			boardString += board[i][j]
+		}
+		boardString += "\n"
+	}
+	return boardString
+}
--- main.go
+++ main.go
@@ -1,30 +1,45 @@
 package main
 
 import (
+	"flag"
+	"github.com/hutchisont/conways_game_of_life_tui/internal/board"
 	"github.com/rivo/tview"
+	"os"
 	"time"
 )
 
 func main() {
-	var board [][]string = make([][]string, 50)
-	for i := range board {
-		board[i] = make([]string, 50)
+	var rows, cols int
+	var tickTime time.Duration
+	var help bool
+	flag.IntVar(&rows, "rows", 50, "number of rows")
+	flag.IntVar(&cols, "cols", 50, "number of columns")
+	flag.DurationVar(&tickTime, "tick", 100*time.Millisecond, "time between updates in milliseconds")
+	flag.BoolVar(&help, "help", false, "display help")
+
+	flag.Parse()
+
+	if help {
+		flag.CommandLine.SetOutput(os.Stdout)
+		flag.PrintDefaults()
+		return
 	}
-	initializeBoard(board)
+
+	gameBoard := board.New(rows, cols)
 	app := tview.NewApplication()
 	textView := tview.NewTextView().
-		SetSize(50, 50)
+		SetSize(rows, cols)
 
-	ticker := time.NewTicker(100 * time.Millisecond)
+	ticker := time.NewTicker(tickTime)
 	defer ticker.Stop()
 
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				updateBoard(board)
+				board.Update(gameBoard)
 				app.QueueUpdateDraw(func() {
-					textView.SetText(boardAsString(board))
+					textView.SetText(board.AsString(gameBoard))
 				})
 			}
 		}
@@ -33,72 +48,4 @@ func main() {
 	if err := app.SetRoot(textView, true).Run(); err != nil {
 		panic(err)
 	}
-}
-
-func initializeBoard(board [][]string) {
-	for i := range board {
-		for j := range board[i] {
-			board[i][j] = " "
-		}
-	}
-	board[1][2] = "X"
-	board[2][3] = "X"
-	board[3][1] = "X"
-	board[3][2] = "X"
-	board[3][3] = "X"
-	board[4][2] = "X"
-	board[4][3] = "X"
-	board[5][2] = "X"
-}
-
-func getNeighbors(board [][]string, x int, y int) int {
-	var neighbors int
-	for i := -1; i < 2; i++ {
-		for j := -1; j < 2; j++ {
-			if i == 0 && j == 0 {
-				continue
-			}
-			if x+i < 0 || x+i >= len(board) || y+j < 0 || y+j >= len(board[0]) {
-				continue
-			}
-			if board[x+i][y+j] == "X" {
-				neighbors++
-			}
-		}
-	}
-	return neighbors
-}
-
-// Any live cell with fewer than two live neighbors dies, as if by underpopulation.
-// Any live cell with two or three live neighbors lives on to the next generation.
-// Any live cell with more than three live neighbors dies, as if by overpopulation.
-// Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-
-func updateBoard(board [][]string) {
-	for i := range board {
-		for j := range board[i] {
-			neighbors := getNeighbors(board, i, j)
-			switch {
-			case neighbors < 2:
-				board[i][j] = " "
-			case neighbors == 2:
-				continue
-			case neighbors < 4:
-				board[i][j] = "X"
-			default:
-				board[i][j] = " "
-			}
-		}
-	}
-}
-
-func boardAsString(board [][]string) string {
-	var boardString string
-	for i := range board {
-		for j := range board[i] {
-			boardString += board[i][j]
-		}
-		boardString += "\n"
-	}
-	return boardString
 }
