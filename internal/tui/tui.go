cdaf76219df52c358ca519a5688575696efb075d
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-03-24 04:11:17 -0500

    refactor: move TUI work to its own package

---
+++ internal/tui/tui.go
@@ -0,0 +1,145 @@
+package tui
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/hutchisont/conways_game_of_life_tui/internal/board"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+type TUI struct {
+	app       *tview.Application
+	pages     *tview.Pages
+	menu      *tview.Modal
+	config    *tview.Form
+	boardView *tview.TextView
+	rows      int
+	cols      int
+	gameBoard [][]string
+	tickTime  time.Duration
+}
+
+func New(rows, cols int, tickTime time.Duration) *TUI {
+	t := TUI{
+		rows:     rows,
+		cols:     cols,
+		tickTime: tickTime,
+	}
+
+	t.app = tview.NewApplication()
+	t.pages = tview.NewPages()
+	t.initMenu()
+	t.initConfig()
+
+	t.gameBoard = board.NewBlank(t.rows, t.cols)
+
+	t.boardView = tview.NewTextView().
+		SetSize(t.rows, t.cols).
+		SetText(board.AsString(t.gameBoard))
+
+	t.pages.AddPage("menu", t.menu, true, true)
+	t.pages.AddPage("config", t.config, true, false)
+	t.pages.AddPage("board", t.boardView, true, false)
+
+	t.app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyESC {
+			t.pages.SwitchToPage("menu")
+		}
+		return event
+	})
+
+	return &t
+}
+
+func (t *TUI) Run() error {
+	oldTick := t.tickTime
+	ticker := time.NewTicker(t.tickTime)
+	defer ticker.Stop()
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				if name, item := t.pages.GetFrontPage(); item != nil && name == "board" {
+					board.Update(t.gameBoard)
+					t.app.QueueUpdateDraw(func() {
+						t.boardView.SetText(board.AsString(t.gameBoard))
+					})
+				}
+				if t.tickTime > 0 && t.tickTime != oldTick {
+					oldTick = t.tickTime
+					ticker.Reset(t.tickTime)
+				}
+			}
+		}
+	}()
+
+	if err := t.app.SetRoot(t.pages, true).Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *TUI) initMenu() {
+	t.menu = tview.NewModal().
+		SetText("Conway's Game of Life").
+		AddButtons([]string{"Play", "Config", "Quit"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			switch buttonLabel {
+			case "Play":
+				t.pages.SwitchToPage("board")
+			case "Config":
+				t.pages.SwitchToPage("config")
+			case "Quit":
+				t.app.Stop()
+			}
+		})
+}
+
+func (t *TUI) initConfig() {
+	t.config = tview.NewForm()
+	t.config.AddInputField("Rows", "50", 10,
+		func(textToCheck string, lastChar rune) bool {
+			_, err := strconv.Atoi(textToCheck)
+			return err == nil
+		},
+		func(newRows string) {
+			r, err := strconv.Atoi(newRows)
+			if err == nil {
+				t.rows = r
+			}
+		})
+	t.config.AddInputField("Columns", "50", 10,
+		func(textToCheck string, lastChar rune) bool {
+			_, err := strconv.Atoi(textToCheck)
+			return err == nil
+		},
+		func(newCols string) {
+			c, err := strconv.Atoi(newCols)
+			if err == nil {
+				t.cols = c
+			}
+		})
+	t.config.AddInputField("Tick Time (ms)", "16", 10,
+		func(textToCheck string, lastChar rune) bool {
+			_, err := strconv.Atoi(textToCheck)
+			return err == nil
+		},
+		func(newTick string) {
+			tick, err := strconv.Atoi(newTick)
+			if err == nil {
+				t.tickTime = time.Duration(tick) * time.Millisecond
+			}
+		})
+	t.config.AddButton("Done", func() {
+		t.gameBoard = board.NewRPentomino(t.rows, t.cols)
+		if t.boardView != nil {
+			t.boardView.SetSize(t.rows, t.cols).SetText(board.AsString(t.gameBoard))
+		}
+		t.pages.SwitchToPage("menu")
+	})
+}
--- main.go
+++ main.go
@@ -3,93 +3,20 @@ package main
 import (
 	"flag"
 	"os"
-	"strconv"
 	"time"
 
-	"github.com/gdamore/tcell/v2"
-	"github.com/hutchisont/conways_game_of_life_tui/internal/board"
-	"github.com/rivo/tview"
+	"github.com/hutchisont/conways_game_of_life_tui/internal/tui"
 )
-
-var (
-	rows, cols int
-	tickTime   time.Duration
-	help       bool
-
-	gameBoard [][]string
-
-	app       *tview.Application
-	pages     *tview.Pages
-	menu      *tview.Modal
-	config    *tview.Form
-	boardView *tview.TextView
-)
-
-func initMenu() *tview.Modal {
-	return tview.NewModal().
-		SetText("Conway's Game of Life").
-		AddButtons([]string{"Play", "Config", "Quit"}).
-		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-			switch buttonLabel {
-			case "Play":
-				pages.SwitchToPage("board")
-			case "Config":
-				pages.SwitchToPage("config")
-			case "Quit":
-				app.Stop()
-			}
-		})
-}
-
-func initConfig() *tview.Form {
-	form := tview.NewForm()
-	form.AddInputField("Rows", "50", 10,
-		func(textToCheck string, lastChar rune) bool {
-			_, err := strconv.Atoi(textToCheck)
-			return err == nil
-		},
-		func(newRows string) {
-			r, err := strconv.Atoi(newRows)
-			if err == nil {
-				rows = r
-			}
-		})
-	form.AddInputField("Columns", "50", 10,
-		func(textToCheck string, lastChar rune) bool {
-			_, err := strconv.Atoi(textToCheck)
-			return err == nil
-		},
-		func(newCols string) {
-			c, err := strconv.Atoi(newCols)
-			if err == nil {
-				cols = c
-			}
-		})
-	form.AddInputField("Tick Time (ms)", "16", 10,
-		func(textToCheck string, lastChar rune) bool {
-			_, err := strconv.Atoi(textToCheck)
-			return err == nil
-		},
-		func(newTick string) {
-			t, err := strconv.Atoi(newTick)
-			if err == nil {
-				tickTime = time.Duration(t) * time.Millisecond
-			}
-		})
-	form.AddButton("Done", func() {
-		gameBoard = board.NewRPentomino(rows, cols)
-		if boardView != nil {
-			boardView.SetSize(rows, cols).SetText(board.AsString(gameBoard))
-		}
-		pages.SwitchToPage("menu")
-	})
-	return form
-}
 
 func main() {
+	var (
+		rows, cols int
+		tickTime   time.Duration
+		help       bool
+	)
 	flag.IntVar(&rows, "rows", 50, "number of rows")
 	flag.IntVar(&cols, "cols", 50, "number of columns")
-	flag.DurationVar(&tickTime, "tick", 100*time.Millisecond, "time between updates in milliseconds")
+	flag.DurationVar(&tickTime, "tick", 16*time.Millisecond, "time between updates in milliseconds")
 	flag.BoolVar(&help, "help", false, "display help")
 
 	flag.Parse()
@@ -100,50 +27,8 @@ func main() {
 		return
 	}
 
-	gameBoard = board.NewRPentomino(rows, cols)
-	app = tview.NewApplication()
-	pages = tview.NewPages()
-	menu = initMenu()
-	config = initConfig()
-
-	boardView = tview.NewTextView().
-		SetSize(rows, cols).
-		SetText(board.AsString(gameBoard))
-
-	pages.AddPage("menu", menu, true, true)
-	pages.AddPage("config", config, true, false)
-	pages.AddPage("board", boardView, true, false)
-
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyESC {
-			pages.SwitchToPage("menu")
-		}
-		return event
-	})
-
-	oldTick := tickTime
-	ticker := time.NewTicker(tickTime)
-	defer ticker.Stop()
-
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				if name, item := pages.GetFrontPage(); item != nil && name == "board" {
-					board.Update(gameBoard)
-					app.QueueUpdateDraw(func() {
-						boardView.SetText(board.AsString(gameBoard))
-					})
-				}
-				if tickTime > 0 && tickTime != oldTick {
-					oldTick = tickTime
-					ticker.Reset(tickTime)
-				}
-			}
-		}
-	}()
-
-	if err := app.SetRoot(pages, true).Run(); err != nil {
+	t := tui.New(rows, cols, tickTime)
+	if err := t.Run(); err != nil {
 		panic(err)
 	}
 }
